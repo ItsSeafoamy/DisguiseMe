ab15949eebe32348f4bf373639762ca5e71e154d
Laxynd <laxynd@siblaze.com>
2016-03-23 17:13:58 +0000

    Added Endermite and Silverfish

---
+++ src/main/java/me/laxynd/disguiseme/disguisetype/DisguiseEndermite.java
@@ -0,0 +1,11 @@
+package me.laxynd.disguiseme.disguisetype;
+
+import org.bukkit.entity.EntityType;
+
+public class DisguiseEndermite extends DisguiseMonster {
+
+	@Override
+	public EntityType getEntityType() {
+		return EntityType.ENDERMITE;
+	}
+}
---
+++ src/main/java/me/laxynd/disguiseme/disguisetype/DisguiseSilverfish.java
@@ -0,0 +1,11 @@
+package me.laxynd.disguiseme.disguisetype;
+
+import org.bukkit.entity.EntityType;
+
+public class DisguiseSilverfish extends DisguiseMonster {
+
+	@Override
+	public EntityType getEntityType() {
+		return EntityType.SILVERFISH;
+	}
+}
